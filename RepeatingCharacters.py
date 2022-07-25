a99b2fc8be074d6a5bba77640f75e26f3b5b74a1
Elsazach <61414366+Elsazach@users.noreply.github.com>
2022-07-25 13:16:11 +0530

    Update RepeatingCharacters.py

--- RepeatingCharacters.py
+++ RepeatingCharacters.py
@@ -1,9 +1,17 @@
-print("enter th string?")
-count=0
-str1=input()
-print("enter the character ?")
-ch=input()
-for i in str1:
-    if(str1.count(i)>=1):
-        str2=str1.replace(i,"")
-print(str2)
+print("Enter a String: ")
+str = input()
+strlist = []
+strlist=list(str)
+newlist = []
+j = 0
+for i in range(len(strlist)):
+    if str[i] not in newlist:
+        newlist.insert(j, str[i])
+        j = j+1
+
+str = ""
+str = str.join(newlist)
+print(str)
+
+
+        
